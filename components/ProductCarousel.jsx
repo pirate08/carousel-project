319674fd964a80cb6a0ec3a665e8a0a8d519100a
Mayukh <mayukhdebgoswami82@gmail.com>
2024-05-16 20:01:13 +0530

    Ready to work..

---
+++ components/ProductCarousel.jsx
@@ -0,0 +1,27 @@
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const ProductCarousel = ({ title, products }) => {
+  return (
+    <div className='my-8'>
+      <h2 className='text-2xl font-bold mb-4'>{title}</h2>
+      {products && products.length > 0 ? (
+        <Carousel>
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className='p-10  bg-black text-white shadow-md rounded-lg'>
+              <h3 className='text-xl font-semibold'>{product.name}</h3>
+              <p>Price: {product.price}</p>
+              <p>Currency: {product.currency}</p>
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <div>No products available</div>
+      )}
+    </div>
+  );
+};
+
+export default ProductCarousel;
